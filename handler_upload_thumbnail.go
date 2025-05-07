a927dfddb8146bba24f942d2d5f248c9d0bd07c8
coderjcronin <coderjmc@gmail.com>
2025-05-07 15:02:13 -0400

    Completed 1st Chapter

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +33,66 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to prase form file", err)
+		return
+	}
+	mediaType := header.Header.Get("Content-Type")
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse image data", err)
+		return
+	}
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to lookup video by ID", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	var fileExt string
+
+	fileExt, valid := strings.CutPrefix(mediaType, "image/")
+	if !valid {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type, must be image", nil)
+		return
+	}
+	imageFileName := fmt.Sprintf("%s.%s", videoID.String(), fileExt)
+	imageFilePath := filepath.Join(cfg.assetsRoot, imageFileName)
+
+	imageFile, err := os.Create(imageFilePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating thumbnail file", err)
+		return
+	}
+	defer imageFile.Close()
+
+	_, err = imageFile.Write(imageData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error writing to thumbnail file", err)
+		return
+	}
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, imageFileName)
+
+	newVideoMetaData := database.Video{
+		ID:                videoMetaData.ID,
+		CreatedAt:         videoMetaData.CreatedAt,
+		UpdatedAt:         videoMetaData.UpdatedAt,
+		ThumbnailURL:      &thumbnailUrl,
+		VideoURL:          videoMetaData.VideoURL,
+		CreateVideoParams: videoMetaData.CreateVideoParams,
+	}
+
+	err = cfg.db.UpdateVideo(newVideoMetaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video data", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, newVideoMetaData)
 }
